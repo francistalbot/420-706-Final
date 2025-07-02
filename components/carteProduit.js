379108892dc789a8cf0fd828e475bd382b8c3c7b
Francis-Laptop <francis.talbot@hotmail.com>
2025-07-02 12:02:25 -0400

    add carteProduit

--- components/carteProduit.js
+++ components/carteProduit.js
@@ -1,15 +1,10 @@
-export const carteProduit = () => {
+export const carteProduit = (produit) => {
   return ` 
   <div class="produits-grid">
         <div class="carte-produit">
-            <h4>Produit 1</h4>
-            <p>Description du produit 1</p>
-            <span class="prix">30$</span>
-        </div>
-        <div class="carte-produit">
-            <h4>Produit 2</h4>
-            <p>Description du produit 2</p>
-            <span class="prix">40$</span>
+            <h4>${produit.nom}</h4>
+            <p>${produit.description}</p>
+            <span class="prix">${produit.prix}</span>
         </div>
     </div>
     `;
